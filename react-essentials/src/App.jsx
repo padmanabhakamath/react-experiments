e7d3c153ff1281cf9a0f3d564e37b0efdd8b50e6
padmanabhakamath <padmanabha.kamath@gmail.com>
2025-11-18 01:15:44 -0600

    Using state and React hook

--- react-essentials/src/App.jsx
+++ react-essentials/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { CORE_CONCEPTS } from './data';
 import Header from './components/Header/Header'
 import CoreConcept from './components/CoreConcept';
@@ -5,6 +6,20 @@ import TabButton from './components/TabButton.jsx';
 
 
 function App() {
+
+  // setSelectedTopic is a FUNCTION
+  // selectedTopic is a VALUE
+  // argument to useState is the INITIAL VALUE OF SELECTEDTOPIC
+  const [selectedTopic, setSelectedTopic] = useState('Please click a button'); // Must be called directly inside 
+
+  function handleSelect(selectedButton) {
+    // selectedButton => 'components', 'JSX', 'props', 'state'
+    setSelectedTopic(selectedButton)
+    console.log(selectedTopic);
+  }
+
+  console.log('App component executing')
+
   return (
     <div>
       <Header />
@@ -21,11 +36,12 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
             <menu>
-              <TabButton>Children</TabButton>
-              <TabButton>JSX</TabButton>
-              <TabButton>Props</TabButton>
-              <TabButton>State</TabButton>
-            </menu>
+              <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+              <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+              <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+              <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+            </menu>   
+            {selectedTopic}       
         </section>
       </main>
     </div>
--- react-essentials/src/components/TabButton.jsx
+++ react-essentials/src/components/TabButton.jsx
@@ -1,6 +1,10 @@
-// export default function TabButton({children}) { // object destructuring
-export default function TabButton(props) {
-    return <li><button>{props.children}</button></li>
+
+export default function TabButton({ children, onSelect}) { // object destructuring
+    console.log(' TAB BUTTON executing')
+    return (<li>
+        <button onClick={onSelect}>{children}</button>
+        </li>
+    );
 }
 
 // export default function TabButton(props) {

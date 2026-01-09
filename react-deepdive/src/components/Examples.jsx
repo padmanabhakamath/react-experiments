f4af13ea2a8382dab7708ed8c6a4debed062abbe
padmanabhakamath <padmanabha.kamath@gmail.com>
2026-01-08 19:24:39 -0600

    added tic-tac-toe project

--- react-deepdive/src/components/Examples.jsx
+++ react-deepdive/src/components/Examples.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { EXAMPLES } from "../data";
 import TabButton from './TabButton.jsx'
+import Tabs from './Tabs.jsx';
 import Section from './Section.jsx';
 
 export default function Examples() {
@@ -26,33 +27,41 @@ export default function Examples() {
     );
   }
     return (
-    <Section title="Examples" id="examples">          
+    <Section title="Examples" id="examples">  
+      <Tabs
+        buttons={
+          <>
+            <TabButton
+                isSelected={selectedTopic === 'components'}
+                onSelect={() => handleSelect('components')}
+              >
+                Components
+              </TabButton>
+              <TabButton
+                isSelected={selectedTopic === 'jsx'}
+                onSelect={() => handleSelect('jsx')}
+              >
+                JSX
+              </TabButton>
+              <TabButton
+                isSelected={selectedTopic === 'props'}
+                onSelect={() => handleSelect('props')}
+              >
+                Props
+              </TabButton>
+              <TabButton
+                isSelected={selectedTopic === 'state'}
+                onSelect={() => handleSelect('state')}
+              >
+                State
+              </TabButton>
+          </>
+        }>
+        {tabContent}
+      </Tabs>        
           <menu>
-            <TabButton
-              isSelected={selectedTopic === 'components'}
-              onSelect={() => handleSelect('components')}
-            >
-              Components
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === 'jsx'}
-              onSelect={() => handleSelect('jsx')}
-            >
-              JSX
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === 'props'}
-              onSelect={() => handleSelect('props')}
-            >
-              Props
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === 'state'}
-              onSelect={() => handleSelect('state')}
-            >
-              State
-            </TabButton>
+            
           </menu>
-          {tabContent}
+          
      </Section>);
 }
---
+++ tic-tac-toe/src/components/Player.jsx
@@ -0,0 +1,9 @@
+export default function Player() {
+    <li>
+          <span className="player">
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">X</span>
+          </span>
+          <button>Edit</button>
+        </li>
+}
